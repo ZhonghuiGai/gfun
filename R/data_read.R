903e44d20b187c2c38d62f5d228e5e3cfc6ae890
ZH Gai <zhgai@aliyun.com>
2023-01-31 13:26:21 +0800

    read.gff3

--- R/data_read.R
+++ R/data_read.R
@@ -14,7 +14,8 @@ data_read <- function(path, rowname = TRUE, colname = TRUE, sheet = 1,
                 "txt" = .read_txt(path, rowname, colname, ...),
                 "csv" = .read_csv(path, rowname, colname, ...),
                 "xlsx" = .read_xlsx(path, rowname, colname, sheet, ...),
-                "jpg" = .read_jpg(path, ...))
+                "jpg" = .read_jpg(path, ...),
+                "gff3" = .read_gff3(path, ...))
   if (verbose) {
     empty_cols <- empty_columns(out)
     if (length(empty_cols)) {
@@ -27,7 +28,7 @@ data_read <- function(path, rowname = TRUE, colname = TRUE, sheet = 1,
 .file_ext <- function(x) {
   pos <- regexpr("\\.([[:alnum:]]+)$", x)
   ifelse(pos > -1L, substring(x, pos + 1L), "")
-} #tools::file_ext
+} # tools::file_ext
 .read_txt <- function(path, rowname = TRUE, colname = TRUE, ...){
  out <- read.table(file = path, header = colname, sep = "\t", quote = "\"",
                    fill = TRUE,  dec = ".", comment.char = "",
@@ -52,4 +53,41 @@ data_read <- function(path, rowname = TRUE, colname = TRUE, sheet = 1,
     annotation_raster(raster = jpg, xmin = -Inf, xmax = Inf, ymin = -Inf, ymax = Inf) +
     theme_nothing()
   return(p)
+}
+# https://www.ncbi.nlm.nih.gov/datasets/docs/v2/reference-docs/file-formats/about-ncbi-gff3/
+.read_gff3 <- function(gff3file){
+  demo <- file(gff3file, open = "r")
+  n <- 1
+  res <- c()
+  while(TRUE){
+    line <-  readLines(demo, n = 1)
+    if(grepl("^##FASTA", line)) break
+    if(length(line) == 0) break
+    if(grepl("^##", line) | grepl("^#!", line)) line <- NULL
+      else if(grepl("gbkey=Src", line) & grepl("region", line)) line <- NULL
+    line <- gsub("^#", "", line)
+    res <- c(res, line)
+    n <- n + 1
+  }
+  close(demo) # end of reading, and start cleaning
+  resu <- lapply(res, strsplit, split = "\t")
+  result <- matrix(NA, nrow = length(resu), ncol = 9)
+  result <- as.data.frame(result)
+  colnames(result) <- c("seqid", "source", "type", "start", "end",
+                        "score", "strand", "phase", "attributes")
+  for (i in 1:length(resu)) result[i, ] <- resu[[i]][[1]]
+  result$ID <- .get_attribute_field(result, "ID")
+  result$Name <- .get_attribute_field(result, "Name")
+  result$product <- .get_attribute_field(result, "product")
+  return(result)
+}
+.get_attribute_field <- function(x, field){
+  s <- strsplit(x$attributes, split = ";", fixed = TRUE)
+  return(sapply(s, .find_key_value, field))
+}
+.find_key_value <- function(atts, field){
+  a <- strsplit(atts, split = "=", fixed = TRUE)
+  m <- match(field, sapply(a, "[", 1))
+  if (!is.na(m)) rv <- a[[m]][2] else rv <- as.character(NA)
+  return(rv)
 }
